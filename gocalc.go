303a873c86e74f56ef2ac2cdeb0b088c1b563702
Dom <dom_brownuk@icloud.com>
2024-09-22 20:51:18 +0100

    Initial commit

---
+++ gocalc.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstnum float64
+	var secondnum float64
+
+	fmt.Println("Please enter your first number")
+	fmt.Scan(firstnum)
+
+	fmt.Println("Please enter your second number")
+	fmt.Scan(secondnum)
+
+}
