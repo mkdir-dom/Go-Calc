dcd66e4830a37e1f83173d6ba85b198dae2deb48
Dom <dom_brownuk@icloud.com>
2024-09-22 21:20:09 +0100

    New code added

--- gocalc.go
+++ gocalc.go
@@ -5,11 +5,25 @@ import "fmt"
 func main() {
 	var firstnum float64
 	var secondnum float64
+	var operator string
+
+	resulttimes := times(firstnum, secondnum)
 
 	fmt.Println("Please enter your first number")
-	fmt.Scan(firstnum)
+	fmt.Scan(&firstnum)
 
 	fmt.Println("Please enter your second number")
-	fmt.Scan(secondnum)
+	fmt.Scan(&secondnum)
 
+	fmt.Println("Please enter your operator")
+	fmt.Scan(&operator)
+
+	if operator == "*" {
+		fmt.Println(resulttimes)
+	}
+
+}
+
+func times(firstnum, secondnum float64) float64 {
+	return firstnum + secondnum
 }
